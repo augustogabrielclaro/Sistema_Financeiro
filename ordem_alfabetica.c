c4f6a47cd6726b82234b3ae01029322d7421e19b
Pedro Gerhard <pedrogerhardc@gmail.com>
2024-11-17 18:05:27 -0300

    Tela ordem alfabetica

---
+++ ordem_alfabetica.c
@@ -0,0 +1,22 @@
+#include "funcoes.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void ordem_alfabetica(lista_contas *lista_contas) {
+     tela();
+    gotoxy(2,5);
+    printf("Cd Banco");
+    gotoxy(2,6);
+    printf("-- ------------------- ----- -------- -------------- ----------- ----------- -");
+    gotoxy(25,5);
+    printf("Agenc Conta");
+    gotoxy(40,5);
+    printf("Tipo Conta");
+    gotoxy(55,5);
+    printf("Saldo");
+    gotoxy(67,5);
+    printf("Limite");
+    gotoxy(78,5);
+    printf("St");
+    gotoxy(20,30);
+}
